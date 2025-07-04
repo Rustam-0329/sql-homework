68446da990aa4c03cf05083ffc06b844ee2706f1
Rustam-0329 <rustam.gulyamov29@gmail.com>
2025-07-04 20:29:05 +0500

    Update hw2.sql

--- lesson-2/hw2.sql
+++ lesson-2/hw2.sql
@@ -1,1 +1,154 @@
+--Lesson 2: DDL and DML Commands
 
+/*Basic-Level Tasks*/
+
+--1.Create a table Employees with columns: EmpID INT, Name (VARCHAR(50)), and Salary (DECIMAL(10,2)).
+CREATE TABLE Employees(EmpID INT PRIMARY KEY, Name VARCHAR(50), Salary DECIMAL(10,2));
+
+--2.Insert three records into the Employees table using different INSERT INTO approaches (single-row insert and multiple-row insert).
+--single-row insert
+INSERT INTO Employees(EmpID, Name, Salary) VALUES(1,'Rashid Jurayev', 10000)
+INSERT INTO Employees(EmpID, Name, Salary) VALUES(2,'Anton Kostyuchenko', 5250)
+INSERT INTO Employees(EmpID, Name, Salary) VALUES(3,'Doston Abdullayev', 2500)
+--multiple-row insert
+INSERT INTO Employees(EmpID, Name, Salary) 
+VALUES
+(4, 'Abdulla Qurbonov', 9000),
+(5, 'Marat Kholmukhamedov', 2000),
+(6, 'Shahzod Karimov', 3850)
+
+SELECT * FROM Employees
+
+--3.Update the Salary of an employee to 7000 where EmpID = 1.
+UPDATE Employees
+SET Salary = 7000
+WHERE EmpID = 1;
+
+--4.Delete a record from the Employees table where EmpID = 2.
+DELETE FROM Employees
+WHERE EmpID = 2;
+
+--5.Give a brief definition for difference between DELETE, TRUNCATE, and DROP.
+/* 
+DELETE - belongs to DML command, it removes specific rows from a table, keeping the table itself.
+TRUNCATE - belongs to DDL command, it clears all rows from a table, but the table stays empty.
+DROP - belongs to DDL command, it deletes the entire table, including its structure.
+*/
+
+--6.Modify the Name column in the Employees table to VARCHAR(100).
+ALTER TABLE Employees
+ALTER COLUMN Name VARCHAR(100);
+
+--7.Add a new column Department (VARCHAR(50)) to the Employees table.
+ALTER TABLE Employees
+ADD Department VARCHAR(50);
+
+--8.Change the data type of the Salary column to FLOAT.
+ALTER TABLE Employees
+ALTER COLUMN Salary FLOAT;
+
+--9.Create another table Departments with columns DepartmentID (INT, PRIMARY KEY) and DepartmentName (VARCHAR(50)).
+CREATE TABLE Departments (
+	DepartmentID INT PRIMARY KEY,
+	DepartmentName VARCHAR(50)
+);
+SELECT * FROM Departments
+
+--10.Remove all records from the Employees table without deleting its structure.
+TRUNCATE TABLE Employees; 
+
+/*Intermediate-Level Tasks*/
+
+--11.Insert five records into the Departments table using INSERT INTO SELECT method(you can write anything you want as data).
+INSERT INTO Departments (DepartmentID, DepartmentName)
+SELECT 1, 'Finance'
+UNION ALL
+SELECT 2, 'Human Resource'
+UNION ALL
+SELECT 3, 'Marketing'
+UNION ALL
+SELECT 4, 'Sales'
+UNION ALL
+SELECT 5, 'Quality';
+
+--12.Update the Department of all employees where Salary > 5000 to 'Management'.
+INSERT INTO Employees(EmpID, Name, Salary) 
+VALUES
+(1, 'Rashid Jurayev', 10000),
+(2, 'Anton Kostyuchenko', 5250),
+(3, 'Doston Abdullayev', 2500),
+(4, 'Abdulla Qurbonov', 9000),
+(5, 'Marat Kholmukhamedov', 2000),
+(6, 'Shahzod Karimov', 3850);
+
+UPDATE Employees
+SET Department = 'Management'
+WHERE Salary > 5000;
+
+--13.Write a query that removes all employees but keeps the table structure intact.
+TRUNCATE TABLE Employees;
+
+--14.Drop the Department column from the Employees table.
+ALTER TABLE Employees
+DROP COLUMN Department;
+
+--15.Rename the Employees table to StaffMembers using SQL commands.
+EXEC sp_rename 'Employees', 'StaffMembers';
+SELECT * FROM StaffMembers
+
+--16.Write a query to completely remove the Departments table from the database.
+DROP TABLE Departments;
+
+/*Advanced-Level Tasks*/
+
+--17.Create a table named Products with at least 5 columns, including: ProductID (Primary Key), ProductName (VARCHAR), Category (VARCHAR), Price (DECIMAL)
+CREATE TABLE Products (
+	ProductID INT PRIMARY KEY,
+	ProductName VARCHAR(20),
+	Category VARCHAR(20),
+	Price DECIMAL
+);
+
+--18.Add a CHECK constraint to ensure Price is always greater than 0.
+ALTER TABLE Products
+ADD CONSTRAINT CHK_ProductsPrice CHECK (Price > 0);
+
+--19.Modify the table to add a StockQuantity column with a DEFAULT value of 50.
+ALTER TABLE Products
+ADD StockQuantity VARCHAR(50);
+
+--20.Rename Category to ProductCategory
+EXEC sp_rename 'Products.Category', 'ProductCategory', 'COLUMN';
+
+--21.Insert 5 records into the Products table using standard INSERT INTO queries.
+INSERT INTO Products (
+	ProductID,
+	ProductName,
+	ProductCategory,
+	Price,
+	StockQuantity
+)  
+VALUES 
+(1, 'Apple', 'Fruits', 5, 100),
+(2, 'Coca-Cola', 'Drinks', 3, 40),
+(3, 'Tomato', 'Vegatables', 2, 24),
+(4, 'Parlament', 'Tobaco', 8, 45),
+(5, 'Fairy', 'Cleanings', 3, 23);
+
+--22.Use SELECT INTO to create a backup table called Products_Backup containing all Products data.
+SELECT * INTO Products_Backup
+FROM Products;
+
+--23.Rename the Products table to Inventory.
+EXEC sp_rename 'Products', 'Inventory';
+
+--24.Alter the Inventory table to change the data type of Price from DECIMAL(10,2) to FLOAT.
+ALTER TABLE Inventory
+ALTER COLUMN Price FLOAT;
+
+--25.Add an IDENTITY column named ProductCode that starts from 1000 and increments by 5 to Inventory table. 
+ALTER TABLE Inventory
+ADD ProductCode INT IDENTITY(1000, 5)
+
+
+SELECT * FROM Inventory
