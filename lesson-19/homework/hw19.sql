5d683d2e08002128ab11f4726361f80aab7a5770
Rustam-0329 <rustam.gulyamov29@gmail.com>
2025-08-30 16:35:31 +0500

    Update hw19.sql

--- lesson-19/homework/hw19.sql
+++ lesson-19/homework/hw19.sql
@@ -1,1 +1,338 @@
 
+CREATE DATABASE homework19
+GO
+USE homework19;
+
+--Lesson-19: Stored procedures, Merge and Practice
+
+--TASKS on Stored Procedures and MERGE
+--Part 1: Stored Procedure Tasks
+
+CREATE TABLE Employees (
+    EmployeeID INT PRIMARY KEY,
+    FirstName NVARCHAR(50),
+    LastName NVARCHAR(50),
+    Department NVARCHAR(50),
+    Salary DECIMAL(10,2)
+);
+
+CREATE TABLE DepartmentBonus (
+    Department NVARCHAR(50) PRIMARY KEY,
+    BonusPercentage DECIMAL(5,2)
+);
+
+INSERT INTO Employees VALUES
+(1, 'John', 'Doe', 'Sales', 5000),
+(2, 'Jane', 'Smith', 'Sales', 5200),
+(3, 'Mike', 'Brown', 'IT', 6000),
+(4, 'Anna', 'Taylor', 'HR', 4500);
+
+INSERT INTO DepartmentBonus VALUES
+('Sales', 10),
+('IT', 15),
+('HR', 8);
+
+GO
+
+------------------------------------------------------------------------------------------------------
+
+--Task 1:
+--Create a stored procedure that:
+
+--Creates a temp table #EmployeeBonus
+--Inserts EmployeeID, FullName (FirstName + LastName), Department, Salary, and BonusAmount into it
+--(BonusAmount = Salary * BonusPercentage / 100)
+--Then, selects all data from the temp table.
+
+--Solution:
+
+CREATE PROC sp_EmpSalaryWithBonus
+AS
+BEGIN
+	SELECT
+		e.EmployeeID,
+		CONCAT(e.FirstName,' ', e.LastName) AS FullName,
+		e.Department,
+		e.Salary,
+		CAST((e.Salary * COALESCE(b.BonusPercentage, 0) / 100) AS decimal(10,2)) AS BonusAmount
+	INTO
+		#EmployeeBonus
+	FROM Employees e
+	LEFT JOIN DepartmentBonus b ON e.Department = b.Department
+
+	SELECT * 
+	FROM #EmployeeBonus
+END;
+GO
+EXEC sp_EmpSalaryWithBonus
+GO
+
+------------------------------------------------------------------------------------------------------
+
+
+--Task 2:
+--Create a stored procedure that:
+
+--Accepts a department name and an increase percentage as parameters
+--Update salary of all employees in the given department by the given percentage
+--Returns updated employees from that department.
+
+
+CREATE PROC usp_EmpSalaryIncrease
+	@Department VARCHAR(50),
+	@IncreasePercentage DECIMAL(10, 2)
+AS
+BEGIN
+	UPDATE Employees
+	SET Salary = Salary * (1 + @IncreasePercentage / 100)
+	WHERE Department = @Department;
+
+	SELECT
+        EmployeeID,
+        CONCAT(FirstName, ' ', LastName) AS FullName,
+        Department,
+        Salary
+    FROM Employees
+    WHERE Department = @Department;
+END;
+GO
+EXEC usp_EmpSalaryIncrease HR, 8
+
+------------------------------------------------------------------------------------------------------
+
+--Part 2: MERGE Tasks
+--Tables to use:
+
+CREATE TABLE Products_Current (
+    ProductID INT PRIMARY KEY,
+    ProductName NVARCHAR(100),
+    Price DECIMAL(10,2)
+);
+
+CREATE TABLE Products_New (
+    ProductID INT PRIMARY KEY,
+    ProductName NVARCHAR(100),
+    Price DECIMAL(10,2)
+);
+
+INSERT INTO Products_Current VALUES
+(1, 'Laptop', 1200),
+(2, 'Tablet', 600),
+(3, 'Smartphone', 800);
+
+INSERT INTO Products_New VALUES
+(2, 'Tablet Pro', 700),
+(3, 'Smartphone', 850),
+(4, 'Smartwatch', 300);
+
+--Task 3:
+--Perform a MERGE operation that:
+
+--Updates ProductName and Price if ProductID matches
+--Inserts new products if ProductID does not exist
+--Deletes products from Products_Current if they are missing in Products_New
+--Return the final state of Products_Current after the MERGE.
+
+SELECT * FROM Products_Current
+SELECT * FROM Products_New
+
+--Solution:
+
+MERGE INTO Products_Current AS T
+USING Products_New AS S
+ON T.ProductID = S.ProductID
+WHEN MATCHED THEN
+	UPDATE
+	SET T.ProductName = S.ProductName, T.Price = S.Price
+WHEN NOT MATCHED BY TARGET THEN
+	INSERT(ProductID, ProductName, Price) VALUES(S.ProductID, S.ProductName, S.Price)
+WHEN NOT MATCHED BY SOURCE THEN
+	DELETE;
+
+SELECT * FROM Products_Current;
+
+------------------------------------------------------------------------------------------------------
+
+--Task 4:
+--Tree Node
+
+--Each node in the tree can be one of three types:
+
+--"Leaf": if the node is a leaf node.
+--"Root": if the node is the root of the tree.
+--"Inner": If the node is neither a leaf node nor a root node.
+--Write a solution to report the type of each node in the tree.
+
+--Input:
+
+CREATE TABLE Tree (id INT, p_id INT);
+TRUNCATE TABLE Tree;
+INSERT INTO Tree (id, p_id) VALUES (1, NULL);
+INSERT INTO Tree (id, p_id) VALUES (2, 1);
+INSERT INTO Tree (id, p_id) VALUES (3, 1);
+INSERT INTO Tree (id, p_id) VALUES (4, 2);
+INSERT INTO Tree (id, p_id) VALUES (5, 2);
+
+--Output:
+
+--id	type
+--1		Root
+--2		Inner
+--3		Leaf
+--4		Leaf
+--5		Leaf
+
+--Solution:
+
+SELECT *,
+	CASE WHEN p_id IS NULL THEN 'Root'
+		 WHEN id IN (SELECT p_id FROM Tree) THEN 'Inner'
+		 ELSE 'Leaf'
+	END Type
+FROM Tree
+
+
+------------------------------------------------------------------------------------------------------
+
+
+--Task 5:
+--Confirmation Rate
+
+--Find the confirmation rate for each user. If a user has no confirmation requests, the rate should be 0.
+
+--Solution:
+
+SELECT 
+	s.user_id, 
+	ROUND(AVG(CASE WHEN action = 'confirmed' THEN 1 ELSE 0.00 END), 2) AS confirmation_rate  
+FROM Signups s
+LEFT JOIN Confirmations c ON s.user_id = c.user_id
+GROUP BY s.user_id
+
+
+------------------------------------------------------------------------------------------------------
+
+
+--Task 6:
+--Find employees with the lowest salary
+
+--Input:
+DROP TABLE IF EXISTS employees;
+CREATE TABLE employees (
+    id INT PRIMARY KEY,
+    name VARCHAR(100),
+    salary DECIMAL(10,2)
+);
+
+INSERT INTO employees (id, name, salary) VALUES
+(1, 'Alice', 50000),
+(2, 'Bob', 60000),
+(3, 'Charlie', 50000);
+
+--Find all employees who have the lowest salary using subqueries.
+
+--Solutions:
+
+SELECT * 
+FROM employees
+WHERE salary = (SELECT MIN(salary) FROM employees);
+
+
+------------------------------------------------------------------------------------------------------
+
+--Task 7:
+--Get Product Sales Summary
+
+--Input:
+-- Products Table
+CREATE TABLE Products (
+    ProductID INT PRIMARY KEY,
+    ProductName NVARCHAR(100),
+    Category NVARCHAR(50),
+    Price DECIMAL(10,2)
+);
+
+-- Sales Table
+CREATE TABLE Sales (
+    SaleID INT PRIMARY KEY,
+    ProductID INT FOREIGN KEY REFERENCES Products(ProductID),
+    Quantity INT,
+    SaleDate DATE
+);
+
+INSERT INTO Products (ProductID, ProductName, Category, Price) VALUES
+(1, 'Laptop Model A', 'Electronics', 1200),
+(2, 'Laptop Model B', 'Electronics', 1500),
+(3, 'Tablet Model X', 'Electronics', 600),
+(4, 'Tablet Model Y', 'Electronics', 700),
+(5, 'Smartphone Alpha', 'Electronics', 800),
+(6, 'Smartphone Beta', 'Electronics', 850),
+(7, 'Smartwatch Series 1', 'Wearables', 300),
+(8, 'Smartwatch Series 2', 'Wearables', 350),
+(9, 'Headphones Basic', 'Accessories', 150),
+(10, 'Headphones Pro', 'Accessories', 250),
+(11, 'Wireless Mouse', 'Accessories', 50),
+(12, 'Wireless Keyboard', 'Accessories', 80),
+(13, 'Desktop PC Standard', 'Computers', 1000),
+(14, 'Desktop PC Gaming', 'Computers', 2000),
+(15, 'Monitor 24 inch', 'Displays', 200),
+(16, 'Monitor 27 inch', 'Displays', 300),
+(17, 'Printer Basic', 'Office', 120),
+(18, 'Printer Pro', 'Office', 400),
+(19, 'Router Basic', 'Networking', 70),
+(20, 'Router Pro', 'Networking', 150);
+
+INSERT INTO Sales (SaleID, ProductID, Quantity, SaleDate) VALUES
+(1, 1, 2, '2024-01-15'),
+(2, 1, 1, '2024-02-10'),
+(3, 1, 3, '2024-03-08'),
+(4, 2, 1, '2024-01-22'),
+(5, 3, 5, '2024-01-20'),
+(6, 5, 2, '2024-02-18'),
+(7, 5, 1, '2024-03-25'),
+(8, 6, 4, '2024-04-02'),
+(9, 7, 2, '2024-01-30'),
+(10, 7, 1, '2024-02-25'),
+(11, 7, 1, '2024-03-15'),
+(12, 9, 8, '2024-01-18'),
+(13, 9, 5, '2024-02-20'),
+(14, 10, 3, '2024-03-22'),
+(15, 11, 2, '2024-02-14'),
+(16, 13, 1, '2024-03-10'),
+(17, 14, 2, '2024-03-22'),
+(18, 15, 5, '2024-02-01'),
+(19, 15, 3, '2024-03-11'),
+(20, 19, 4, '2024-04-01');
+GO
+
+--Create a stored procedure called GetProductSalesSummary that:
+
+--Accepts a @ProductID input
+--Returns:
+--ProductName
+--Total Quantity Sold
+--Total Sales Amount (Quantity × Price)
+--First Sale Date
+--Last Sale Date
+--If the product has no sales, return NULL for quantity, total amount, first date, and last date, but still return the product name.
+
+CREATE PROC sp_GetProductSalesSummary
+	@ProductID INT
+AS
+BEGIN
+	SELECT 
+		p.ProductName,
+		SUM(s.Quantity) AS Total_Quantity_Sold,
+		SUM(s.Quantity * p.Price) AS Total_Sales_Amount,
+		MIN(s.SaleDate) AS First_Sale_Date,
+		MAX(s.SaleDate) AS Last_Sale_Date
+	FROM Products p
+	JOIN Sales s ON p.ProductID = s.ProductID
+	WHERE p.ProductID = @ProductID
+	GROUP BY p.ProductName
+END;
+GO
+EXEC sp_GetProductSalesSummary 7;
+
+
+
